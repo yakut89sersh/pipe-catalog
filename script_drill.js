941038c19b2cbff786b802aa9c319fb2cc38fd70
yakut89sersh <yakut89sersh@gmail.com>
2025-07-07 22:15:14 +0300

    Add files via upload

--- script_drill.js
+++ script_drill.js
@@ -129,7 +129,7 @@ if (step === 11) {
 // 🔧 ОБРАБОТКА ШАГА "Длина ниппеля под ключ (мм)"
 if (step === 12) {
   const pinSelect = document.getElementById("pin_length");
-  const values = data
+  const values = filtered
     .map(d => parseFloat(d["Pin tong length, mm"]))
     .filter(v => !isNaN(v));
 
@@ -140,6 +140,8 @@ if (step === 12) {
   return;
   
 }
+
+
 
 // 🔧 ОБРАБОТКА ШАГА "Длина муфты под ключ (мм)"
 if (step === 13) {
