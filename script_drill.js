80854ac484404707cb3e2f683e27b99b8cbdd704
yakut89sersh <yakut89sersh@gmail.com>
2025-07-18 20:41:35 +0300

    Add files via upload

--- script_drill.js
+++ script_drill.js
@@ -342,23 +342,6 @@ select.value = ""; // важно: выбрать именно пустое зн�
   // === Обработка select ===
   select.onchange = function () {
 
-// Найдём зависимые поля
-const nippleSelect = document.getElementById("nipple_length");
-const nippleWrapper = document.getElementById("nipple_wrapper");
-
-const couplingSelect = document.getElementById("coupling_length");
-const couplingWrapper = document.getElementById("coupling_wrapper");
-
-// 🔹 Всегда сбрасываем зависимые поля при любом изменении "Длина трубы"
-  nippleSelect.selectedIndex = 0;
-  nippleSelect.disabled = true;
-  nippleWrapper.style.display = "none";
-
-  couplingSelect.selectedIndex = 0;
-  couplingSelect.disabled = true;
-  couplingWrapper.style.display = "none";
-
-
 
     if (this.value === "manual") {
       input.style.display = "inline-block";
