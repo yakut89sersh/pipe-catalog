13a798427603633e044c22a311e5e21637ffc505
yakut89sersh <yakut89sersh@gmail.com>
2025-07-21 22:54:36 +0300

    Add files via upload

--- script_drill.js
+++ script_drill.js
@@ -678,7 +678,8 @@ function activateCustomLengthField(id, values) {
   pipeLengthHidden.value = val.toFixed(2);
 
   // Только если всё валидно — идем дальше
-  stepShow(12);
+  if (id === "pin_length") stepShow(13);
+if (id === "box_length") stepShow(14);
 });
 
 
